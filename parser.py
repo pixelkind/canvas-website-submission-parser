dee51124d82de845426199b97d5cecb8fe037761
Garrit <garrit.schaap@ju.se>
2022-11-04 09:35:21 +0100

    Updated the parser

--- parser.py
+++ parser.py
@@ -5,20 +5,21 @@ from bs4 import BeautifulSoup
 
 dir_path = sys.argv[1]
 if os.path.exists(dir_path):
-  dirlist = os.scandir(os.path.basename(dir_path))
+  dirlist = os.scandir(dir_path)
   csv_file = open('export.csv', 'w')
   writer = csv.writer(csv_file)
   header = ["name", "url"]
   writer.writerow(header)
 
   for path in dirlist:
-    file = open(path, "r")
-    content = file.read()
-    bs = BeautifulSoup(content, "html.parser")
-    url = bs.a["href"]
-    title = bs.head.title.text
-    name = title.split(": ")[1]
-    
-    writer.writerow([name, url])
+    if os.path.exists(path) and not os.path.basename(path).startswith('.'):
+      file = open(path, "r")
+      content = file.read()
+      bs = BeautifulSoup(content, "html.parser")
+      url = bs.a["href"]
+      title = bs.head.title.text
+      name = title.split(": ")[1]
+      
+      writer.writerow([name, url])
 
   file.close()
